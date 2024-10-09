787ffb1f68f82917d32e9ce4727d9134ec705e1c
mauassenco <mau.assenco@gmail.com>
2024-10-09 16:30:44 -0300

    feat: creating Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import "./App.css"
 import Navbar from "./components/Navbar"
+import Highlights from "./components/Highlights"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
+      <Hero />
+      <Highlights />
     </main>
   )
 }
